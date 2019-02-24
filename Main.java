33139adeae62ca60cdedcda0e75a0f02e65a4a9e
akshat goel <akshatishu@gmail.com>
2019-02-24 01:12:46 -0800

    isUnique

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         isUnique iu =  new isUnique();
+        System.out.println(iu.isUnique("akshat"));
+        System.out.println(iu.isUnique("tea"));
     }
 }
--- isUnique.java
+++ isUnique.java
@@ -1,5 +1,14 @@
 package com.starksky.ctci;
 
 public class isUnique {
-
+    public boolean isUnique(String str){
+        char[] strArr = new char[26];
+        for(int i = 0; i < str.length(); i++){
+            strArr[str.charAt(i) - 'a']++;
+            if(strArr[str.charAt(i) - 'a'] > 1){
+                return false;
+            }
+        }
+        return true;
+    }
 }
